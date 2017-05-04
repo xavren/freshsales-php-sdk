b78368903a95d9007422a154c833ecbbe9c1d67c
xavren <renaudin.xavier@gmail.com>
2017-05-04 10:06:54 +0200

    delete DS_STORE FILES

--- update.sh
+++ update.sh
@@ -5,7 +5,8 @@ cd tmp
 wget https://s3.amazonaws.com/analytics-library/freshsales_php.zip -O source.zip
 unzip source.zip
 cp -r Freshsales ../src
-rm -f src/Freshsales/Sample.php
 cd ..
+rm -f src/Freshsales/Sample.php
 rm -rf tmp
+find . -name '.DS_Store' -type f -delete
 php-cs-fixer fix --rules=@Symfony ./src
